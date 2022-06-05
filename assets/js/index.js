4b05476501a62a9a70b0f91be1533c8939a60b9a
carreuky <carreuky@gmail.com>
2022-06-05 22:31:33 +0300

    button hovers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
 document.getElementById("about-me").style.color = "black";
+document.getElementById("proj").style.borderBottom = "thick solid black";
+document.getElementById("proj-p").style.paddingTop = "20px";
+
+const btn = document.getElementById('btn-menu');
+
+btn.addEventListener("click", function () {
+
+    btn.style.backgroundColor = 'yellow'
+    
+    });
